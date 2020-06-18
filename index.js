899f69ad3a94ca1073ecb0b2fa02b06ce12bfe84
m.h4ck <12293095+mh4ck@users.noreply.github.com>
2020-06-18 16:51:41 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const Nightmare = require('nightmare')
+const nightmare = Nightmare({ show: true });
+const url = process.argv[2] || null;
+const shortid = require("shortid")
+const fs = require("fs");
+
+if(!url) {
+  console.log("[Error]: Please enter a valid url to crawl.");
+  console.log("[Usage]: node index.js \"https://xxx.xx/123456/\"");
+  process.exit(0);
+  return;
+}
+
+nightmare
+  .goto(url)
+  .wait(5000)
+  .evaluate(() => {
+    return {
+      title: document.title,
+      html: document.documentElement.innerHTML
+    }
+  })
+  .end()
+  .then((ret) => {
+    fs.writeFileSync("pagelog." + shortid.generate() + ".json", JSON.stringify(ret), "utf8");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
---
+++ readme.md
@@ -0,0 +1,15 @@
+# Tracking ADS shown by a defined url
+
+## 1. Step
+
+```#:> npm install```
+
+## 2. Step
+
+```#:> node index.js "https://url.com/0/00000000"
+
+## Result
+
+The result will be written into a pagelog.xxxx.json file in the following format: 
+
+```{title: "[pagetitle]", html: "[complete html code of the page]"}
